26c399fb3d0ffeedde338bd28a7d02a805edf69b
rttitity <jinwoo25803@gmail.com>
2025-03-12 08:41:52 +0000

    fixed tag 23

---
+++ .env.py
@@ -0,0 +1,3 @@
+CLIENT_SECRET = '14nq6ak7c4eajkhc9ghhokg8pvks3jhdrl7hae90p48hkvtjfqob'
+AUTHORIZE_REDIRECT_URL='http://localhost:5000/authorize'    # cognito 콜백 url을 127.0.0.1 로 설정을 하면 오류가 난다
+KAKAO_API_KEY=6da999f321a9dfe0188e4e0d67739034
